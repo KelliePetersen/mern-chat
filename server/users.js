6d3ceb41927fafb020ca4262126c3f3a1979cef3
KelliePetersen <kelliepetersen@outlook.com>
2020-06-15 14:37:37 +1000

    add removeUsers function

--- server/users.js
+++ server/users.js
@@ -4,14 +4,19 @@ const addUser = ({ id, name, room }) => {
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
   const existingUser = users.find(user => user.name === name && user.room === room);
-  if (existingUser) return { error: 'This username is already taken.' }
+  if (existingUser) {
+    return { error: 'This username is already taken.' }
+  }
   const user = { id, name, room };
   users.push(user);
   return user;
 }
 
-const removeUser = () => {
-
+const removeUser = id => {
+  const index = users.findIndex(user => user.id === id);
+  if (index >= 0) {
+    return users.splice(index, 1)[0];
+  }
 }
 
 const getUser = () => {
