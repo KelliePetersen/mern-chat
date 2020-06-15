cc8ec42945a9c122b966c003b8489bf816463544
KelliePetersen <kelliepetersen@outlook.com>
2020-06-15 14:10:50 +1000

    create join chat form with logic to change name and room state

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
   return (
-    <div>
-      Join
+    <div className="outer-wrapper">
+      <div className="inner-wrapper">
+        <h1 className="heading">Join</h1>
+        <div><input placeholder="" className="input" type="text" onChange={e => setName(e.target.value)} required /></div>
+        <div><input placeholder="" className="input" type="text" onChange={e => setRoom(e.target.value)} required /></div>
+        <Link to={`/chat?name=${name}&room=${room}`} onClick={e => (name && room) ? null : e.preventDefault()}>
+          <button className="button" type="submit">Join now</button>
+        </Link>
+      </div>
     </div>
   )
 }
